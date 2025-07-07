0485ffc6a4f2f97407fc3679b5c8e84f49e9b3d9
FaijanShekh06 <zaduu0301@gmail.com>
2025-07-07 12:08:43 +0530

    num is divisible by 3 or not

--- ternary operator/script.js
+++ ternary operator/script.js
@@ -1,3 +1,7 @@
-let age = 12;
-let isVote = age >= 18 ? "you can vote" : "you can't";
+let num = 18;
+let isdiv = num % 3 == 0 ? "divisible by 3" : "not divisble by 3";
+console.log(isdiv);
+
+let age = 18;
+let isVote = age >= 18 ? "Vote" : "can't vote";
 console.log(isVote);
