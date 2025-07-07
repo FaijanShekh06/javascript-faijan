8a1fe90d1b5c94b99b7b1419ed5700a47d2774ed
FaijanShekh06 <zaduu0301@gmail.com>
2025-07-07 11:54:17 +0530

    Condition Statements

---
+++ conditional statements/script.js
@@ -0,0 +1,13 @@
+let age = 12;
+// if (age >= 18) {
+//   console.log("You can vote");
+// } else {
+//   console.log("you can't vote");
+// }
+// if (age >= 18) {
+//   console.log("You can vote");
+// } else if (age <= 17) {
+//   console.log("you are kid");
+// } else {
+//   console.log("you are again kid");
+// }
