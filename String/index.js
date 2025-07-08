29892b7b645bc24d2201ddefbf64a104609c6a3d
FaijanShekh06 <zaduu0301@gmail.com>
2025-07-08 11:39:40 +0530

    String

---
+++ String/index.js
@@ -0,0 +1,27 @@
+console.log("This is String Tutprials");
+let a = "Faijan";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a.length);
+
+let real_name = "   faijan  ";
+let friend = "chicho";
+console.log(`His name is ${real_name} and His friends name is ${friend}`); //template literal we can insert variable
+
+console.log('ddd"dfd');
+console.log('ddd"dfd');
+
+console.log("jdhkh\nskfjh");
+
+console.log(real_name.toUpperCase());
+console.log(real_name.toLowerCase());
+console.log(real_name.slice(0, 4));
+console.log(real_name.slice(1));
+console.log(real_name.replace("j", "z"));
+console.log(real_name.concat(" and His Friend", friend));
+console.log(real_name.concat(friend));
+console.log(real_name.trim());
