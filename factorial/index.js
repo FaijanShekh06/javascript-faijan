108c4d9f0bdcb42f572799bfb204fb3f20017d00
FaijanShekh06 <zaduu0301@gmail.com>
2025-07-10 12:22:01 +0530

    Factorial

--- factorial/index.js
+++ factorial/index.js
@@ -3,15 +3,24 @@
 //   return a * b;
 // };
 // console.log(arr.reduce(fact));
-function factorial(n) {
-  if (n < 0) {
-    return "number has to be positive.";
-  }
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
-  }
-  return result;
-}
+// function factorial(n) {
+//   if (n < 0) {
+//     return "number has to be positive.";
+//   }
+//   let result = 1;
+//   for (let i = 2; i <= n; i++) {
+//     result *= i;
+//   }
+//   return result;
+// }
 
-console.log(factorial(5));
+// console.log(factorial(5));
+
+function fac(num) {
+  let fact = 1;
+  for (let index = 1; index <= num; index++) {
+    fact = fact * index;
+  }
+  return fact;
+}
+console.log(fac(5));
