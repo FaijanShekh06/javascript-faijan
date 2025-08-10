04e97b3d8e0ca1c95b62ff39fb49cf61d7ce5dfc
FaijanShekh06 <zaduu0301@gmail.com>
2025-08-10 11:55:59 +0530

    New Keyword

---
+++ Master Advanced JS Concepts/newKeyword/script.js
@@ -0,0 +1,7 @@
+//New Keyword
+// new => jab bhi new keyword use hota hai tab hamesha ek blank object apne manme bana lo => { } => for below example { age: 12 }
+function abcd() {
+  this.age = 12;
+}
+new abcd()
+
